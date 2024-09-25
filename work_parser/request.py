202f7b90d23b75f07737cf1e2cd6d1b380b1e390
CodeVampireYes <ar2rfilonenko@gmail.com>
2024-09-25 20:32:13 +0200

    first pareser

---
+++ work_parser/request.py
@@ -0,0 +1,15 @@
+from time import sleep
+import requests
+from fake_useragent import UserAgent
+
+
+class RequestEngine:
+
+    def get_response(self, url: str, params: dict | None = None) -> requests.Response:
+        user_agent = UserAgent(platforms=["pc"])
+
+        response = requests.get(url, params=params, headers={"User-Agent": user_agent.random})
+
+        sleep(0.5)
+
+        return response
